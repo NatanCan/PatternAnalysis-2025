1f1b9e556c4017057001ae25ff83109f5cf29e23
NatanCan <nathanc89614@gmail.com>
2025-10-18 17:47:14 +1000

    Created 3 new arrays to store the losses from training and validating as well as the accuracy of the validated set for each epoch. Copied training loop that was used in demo 2 and adapted variables to reflect the variables set in the current train.py file.

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 #This python file defines how the model will train and validate itself using the dataset class defined in dataset.py and include the relevant parameters
 import torch
 import torch.nn as nn
+import os
 import torch.optim as optim
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
@@ -32,4 +33,21 @@ model = ADNIConvNeXt(in_features=1, out_features=2, kernel_size=3)
 criteria = nn.CrossEntropyLoss()
 totStep = len(train_loader)
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+#storing loss values and validation accuracy
+train_loss, val_loss, val_accs = [],[],[]
+
+for epoch in range(Epochs):
+    model.train()
+    running_loss = 0.0
+    for i, (images, labels) in enumerate(train_loader):
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criteria(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    epoch_loss = running_loss / totStep
+    train_loss.append(epoch_loss)
+    print(f"Epoch [{epoch+1}/{Epochs}], Training Loss: {epoch_loss:.4f}")
 
