b8c24723d53b9e5eccfdb0943ddca1b193ed3869
NatanCan <nathanc89614@gmail.com>
2025-10-21 09:48:58 +1000

    Previous message was for this commit of dataset, the previous commit had a few issues like removing self for a function that no longer exist (?) I'm not sure what the thought process was for the git commit in the last itteration.

--- dataset.py
+++ dataset.py
@@ -1,47 +1,29 @@
 import torch
 from torchvision import datasets as dsets
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, ConcatDataset
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from PIL import Image
 import os
 
-b_size = 256
 class ADNIDataset(Dataset):
-    def __init__(self, img_dir, transform=None):
+    def __init__(self, img_dir, b_size=32):
         self.img_dir = img_dir
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        img_pth = os.path.join(self.img_dir, self.data.iloc[idx, 0])
-        image = Image.open(img_pth).convert("RGB")
-        label = torch.tensor(int(self.data.iloc[idx, 1]))
-
-        if self.transform:
-            image = self.transform(image)
-
-        return image, label
-    
-    # Transforms for ConvNeXt input
-    def get_transforms(self):
-        return transforms.Compose([
+        self.b_size = b_size
+        self.transform = transforms.Compose([
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                 std=[0.229, 0.224, 0.225])
         ])
-    
 
     def com_dsets(self, path):
         ADset = dsets.ImageFolder(root=f"{path}\\train\\AD", transform=self.get_transforms())
         NCset = dsets.ImageFolder(root=f"{path}\\train\\NC", transform=self.get_transforms())
-        fullTrain = torch.concat((ADset, NCset), 0)
+        fullTrain = ConcatDataset((ADset, NCset), 0)
         ADteset = dsets.ImageFolder(root=f"{path}\\test\\AD", transform=self.get_transforms())
         NCteset = dsets.ImageFolder(root=f"{path}\\test\\NC", transform=self.get_transforms())
-        fullTest = torch.concat((ADteset, NCteset), 0) 
+        fullTest = ConcatDataset((ADteset, NCteset), 0) 
         return fullTrain, fullTest
 
     def load_data(self, path):
@@ -50,7 +32,7 @@ class ADNIDataset(Dataset):
         val_size = len(TrainSet) - train_size
         train_dset, val_dset = torch.utils.data.random_split(TrainSet, [train_size, val_size])
         #Creating the data Loaders (Copied from part 3)
-        train_loader = DataLoader(dataset=train_dset, batch_size=b_size, shuffle=True)
-        val_loader = DataLoader(dataset=val_dset, batch_size=b_size, shuffle=False)
-        test_loader = DataLoader(dataset=TestSet, batch_size=b_size, shuffle=False) 
+        train_loader = DataLoader(dataset=train_dset, batch_size=self.b_size, shuffle=True)
+        val_loader = DataLoader(dataset=val_dset, batch_size=self.b_size, shuffle=False)
+        test_loader = DataLoader(dataset=TestSet, batch_size=self.b_size, shuffle=False) 
         return train_loader, val_loader, test_loader
