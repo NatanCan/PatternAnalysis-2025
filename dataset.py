710b3ca34dddc0b40af0437eae2f23d87efa3138
NatanCan <nathanc89614@gmail.com>
2025-10-20 21:43:06 +1000

    Realised that the test set also had to be concatanated so included that into the com_dsets() function. With this now i can create a validation set to check against the trained model which is reflected in the added part of the load_data() funciton.

--- dataset.py
+++ dataset.py
@@ -5,7 +5,7 @@ from torchvision import transforms
 from PIL import Image
 import os
 path = r"C:\Users\Nathan\Documents\AD_NC"
-batch_size = 256
+b_size = 256
 class ADNIDataset(dsets):
     def __init__(self, img_dir, transform=None):
         self.img_dir = img_dir
@@ -38,15 +38,18 @@ class ADNIDataset(dsets):
         ADset = dsets.ImageFolder(root=f"{path}\\train\\AD", transform=self.get_transforms())
         NCset = dsets.ImageFolder(root=f"{path}\\train\\NC", transform=self.get_transforms())
         fullTrain = torch.concat((ADset, NCset), 0)
-        return fullTrain
+        ADteset = dsets.ImageFolder(root=f"{path}\\test\\AD", transform=self.get_transforms())
+        NCteset = dsets.ImageFolder(root=f"{path}\\test\\NC", transform=self.get_transforms())
+        fullTest = torch.concat((ADteset, NCteset), 0) 
+        return fullTrain, fullTest
 
     def load_data(self, path):
-        fTrainSet = dsets.ImageFolder(root=f"{path}\\train", transform=self.get_transforms())
-        TestSet = dsets.ImageFolder(root=f"{path}\\test", transform=self.get_transforms())
-
-
+        TrainSet, TestSet = self.com_dsets()
+        train_size = int(0.8*len(TrainSet))
+        val_size = len(TrainSet) - train_size
+        train_dset, val_dset = torch.utils.data.random_split(TrainSet, [train_size, val_size])
         #Creating the data Loaders (Copied from part 3)
-        train_loader = DataLoader(dataset=train_dataset, batch_size=b_size, shuffle=True)
-        #val_loader = DataLoader(dataset=val_dataset, batch_size=b_size, shuffle=False)
+        train_loader = DataLoader(dataset=train_dset, batch_size=b_size, shuffle=True)
+        val_loader = DataLoader(dataset=val_dset, batch_size=b_size, shuffle=False)
         test_loader = DataLoader(dataset=TestSet, batch_size=b_size, shuffle=False) 
-
+        return train_loader, val_loader, test_loader
