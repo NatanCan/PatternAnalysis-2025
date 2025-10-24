69ae504958b3057b9defa4ee5e990de966f4d501
NatanCan <nathanc89614@gmail.com>
2025-10-24 22:17:20 +1000

    Slight modification to the Normalize function in the transform section. In train.py, moved the evaluation into the train model and added the check if cuda is available. After training the model with 18 epochs, both the training and validation loss are very small, indicating that the model is accurate and that the model is not overfitting.

--- dataset.py
+++ dataset.py
@@ -11,10 +11,10 @@ class ADNIDataset(Dataset):
         self.img_dir = img_dir
         self.b_size = b_size
         self.transform = transforms.Compose([
+            transforms.Grayscale(num_output_channels=1),
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                std=[0.229, 0.224, 0.225])
+            transforms.Normalize(mean=[0.5], std=[0.5])
         ])
 
     def get_transforms(self):
--- train.py
+++ train.py
@@ -2,21 +2,28 @@
 import torch
 import torch.nn as nn
 import os
+import time
 import torch.optim as optim
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 
 from modules import ADNIConvNeXt
-from dataset import ADNIDataset, get_transforms
+from dataset import ADNIDataset
 
 #Hyperparameters
-Epochs = 100
-batch_size = 256
+Epochs = 18
+batch_size = 32
 learning_rate = 1e-4
 
+#Checking if GPU is available 
+print("Checking for GPU...")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 #Paths for datasets
-ADNIDset = ADNIDataset(img_dir=r"C:\Users\Nathan\Documents\AD_NC", transform=get_transforms())
-train_loader, val_loader, test_loader = ADNIDset.load_data()
+print("Loading Dataset...")
+path = path = r"C:\Users\Nathan\Documents\AD_NC"
+ADNIDset = ADNIDataset(img_dir=r"C:\Users\Nathan\Documents\AD_NC", b_size=batch_size)
+train_loader, val_loader, test_loader = ADNIDset.load_data(path)
 
 #model
 """def __init__(
@@ -28,29 +35,35 @@ train_loader, val_loader, test_loader = ADNIDset.load_data()
         act = nn.ReLU,
         **kwargs
     ):"""
-
-model = ADNIConvNeXt(in_features=1, out_features=2, kernel_size=3)
+print("Initializing Model...")
+model = ADNIConvNeXt(in_features=1, out_features=2).to(device)
 criteria = nn.CrossEntropyLoss()
 totStep = len(train_loader)
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 #storing loss values and validation accuracy
 train_loss, val_loss, val_accs = [],[],[]
+print("Starting Training...")
 
 for epoch in range(Epochs):
+    start = time.time() #time generation
     model.train()
     running_loss = 0.0
     for i, (images, labels) in enumerate(train_loader):
+        images, labels = images.to(device), labels.to(device)
         optimizer.zero_grad()
         outputs = model(images)
         loss = criteria(outputs, labels)
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
+    if i % 10 == 0:
+        print(f"Batch {i}/{len(train_loader)} | Loss: {loss.item():.4f}")
     epoch_loss = running_loss / totStep
     train_loss.append(epoch_loss)
     print(f"Epoch [{epoch+1}/{Epochs}], Training Loss: {epoch_loss:.4f}")
-
+    end = time.time()
+    print(f"Training took {(end-start)/60:.1f} minutes")
     #Validation phase
     model.eval()
     val_running_loss = 0.0
@@ -58,18 +71,19 @@ for epoch in range(Epochs):
     all_labels = []
     with torch.no_grad():
         for images, labels in val_loader:
+            images, labels = images.to(device), labels.to(device)
             outputs = model(images)
             loss = criteria(outputs, labels)
             val_running_loss += loss.item()
             _, preds = torch.max(outputs, 1)
             all_preds.extend(preds.cpu().numpy())
             all_labels.extend(labels.cpu().numpy())
-    val_epoch_loss = val_running_loss / len(val_loader)
-    val_loss.append(val_epoch_loss)
-    val_acc = accuracy_score(all_labels, all_preds)
-    val_accs.append(val_acc)
-    print(f"Epoch [{epoch+1}/{Epochs}], Validation Loss: {val_epoch_loss:.4f}, Validation Accuracy: {val_acc:.4f}")
-    
+        val_epoch_loss = val_running_loss / len(val_loader)
+        val_loss.append(val_epoch_loss)
+        val_acc = accuracy_score(all_labels, all_preds)
+        val_accs.append(val_acc)
+        print(f"Epoch [{epoch+1}/{Epochs}], Validation Loss: {val_epoch_loss:.4f}, Validation Accuracy: {val_acc:.4f}")
+        
 # Save model
 os.makedirs("checkpoints", exist_ok=True)
 torch.save(model.state_dict(), "checkpoints/convnext_adni.pth")
