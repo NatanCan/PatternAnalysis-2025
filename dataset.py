0bd2ee32c0d7b3e66a9a5bc8d08b531a9aba1319
NatanCan <nathanc89614@gmail.com>
2025-10-30 18:12:18 +1000

    Realised by applying the concattination file it was duplicating and essentially training on the same data twice  the model learns trivial mappings and scores ~1.0 accuracy on validation (since its reusing the same distribution). So removed the concat function and will have to retrain.

--- dataset.py
+++ dataset.py
@@ -1,12 +1,9 @@
 import torch
 from torchvision import datasets as dsets
-from torch.utils.data import Dataset, ConcatDataset
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
-from PIL import Image
-import os
 
-class ADNIDataset(Dataset):
+class ADNIDataset:
     def __init__(self, img_dir, b_size=32):
         self.img_dir = img_dir
         self.b_size = b_size
@@ -20,22 +17,19 @@ class ADNIDataset(Dataset):
     def get_transforms(self):
         return self.transform
     
-    def com_dsets(self, path):
-        ADset = dsets.ImageFolder(root=f"{path}\\train", transform=self.get_transforms())
-        NCset = dsets.ImageFolder(root=f"{path}\\train", transform=self.get_transforms())
-        fullTrain = ConcatDataset((ADset, NCset))
-        ADteset = dsets.ImageFolder(root=f"{path}\\test", transform=self.get_transforms())
-        NCteset = dsets.ImageFolder(root=f"{path}\\test", transform=self.get_transforms())
-        fullTest = ConcatDataset((ADteset, NCteset)) 
-        return fullTrain, fullTest
-
     def load_data(self, path):
-        TrainSet, TestSet = self.com_dsets(path)
-        train_size = int(0.8*len(TrainSet))
-        val_size = len(TrainSet) - train_size
-        train_dset, val_dset = torch.utils.data.random_split(TrainSet, [train_size, val_size])
-        #Creating the data Loaders (Copied from part 3)
-        train_loader = DataLoader(dataset=train_dset, batch_size=self.b_size, shuffle=True)
-        val_loader = DataLoader(dataset=val_dset, batch_size=self.b_size, shuffle=False)
-        test_loader = DataLoader(dataset=TestSet, batch_size=self.b_size, shuffle=False) 
+        # Folders should be structured like:
+        # path/train/AD, path/train/Normal
+        # path/test/AD, path/test/Normal
+        train_dataset = dsets.ImageFolder(root=f"{path}\\train", transform=self.get_transforms())
+        test_dataset = dsets.ImageFolder(root=f"{path}\\test", transform=self.get_transforms())
+
+        # 80/20 train/val split (no duplication)
+        train_size = int(0.8 * len(train_dataset))
+        val_size = len(train_dataset) - train_size
+        train_dset, val_dset = random_split(train_dataset, [train_size, val_size])
+
+        train_loader = DataLoader(train_dset, batch_size=self.b_size, shuffle=True)
+        val_loader = DataLoader(val_dset, batch_size=self.b_size, shuffle=False)
+        test_loader = DataLoader(test_dataset, batch_size=self.b_size, shuffle=False)
         return train_loader, val_loader, test_loader
