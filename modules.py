bb2f4e37d21f0dd8136b51ff6d7ddb7280ef3660
NatanCan <nathanc89614@gmail.com>
2025-10-30 22:09:38 +1000

    "Added ClassificationHead() and ConvNextForImageClassification(), The classification function was to make sure that the number of channels was the number of outputs/classes the images were being classified into (in this case 2). ConvNextForImageClassification() is run to do the actual clasifcaiton of the data."

--- modules.py
+++ modules.py
@@ -9,6 +9,7 @@ from torch import nn
 from torch import Tensor
 from typing import List
 import timm
+import torch
 from torchvision.ops import StochasticDepth
 
 #First implement a resnet model as ConvNeXt starts with a ResNet architecture and then applies a series of transformer-inspired design changes to it.
@@ -22,7 +23,7 @@ class ConvNormAct(nn.Sequential):
         out_features: int,
         kernel_size: int,
         norm = nn.BatchNorm2d,
-        act = nn.ReLU,
+        act = nn.GELU,
         **kwargs
     ):
         super().__init__(
@@ -39,6 +40,10 @@ class ConvNormAct(nn.Sequential):
 
 #Reduces training time and improves generalization by randomly dropping entire layers during training. Method allows for avoiding vanishing gradients and allowing for
 #regularization.
+#VAnishing gradient - a problem in deep neural network training where the gradients become extremely small as they are backpropagated from the output to the earlier layers.
+#regularization - techniques used to prevent overfitting in machine learning models by adding constraints or penalties to the model's complexity.
+from torchvision.ops import StochasticDepth
+
 class LayerScaler(nn.Module):
     def __init__(self, init_value: float, dimensions: int):
         super().__init__()
@@ -54,6 +59,8 @@ class BottleNeck(nn.Module):
         in_features: int,
         out_features: int,
         expansion: int = 4,
+        drop_p: float = .0,
+        layer_scaler_init_value: float = 1e-6,
     ):
         super().__init__()
         expanded_features = out_features * expansion
@@ -70,25 +77,29 @@ class BottleNeck(nn.Module):
             # wide -> narrow
             nn.Conv2d(expanded_features, out_features, kernel_size=1),
         )
-             # If input and output channels differ, use a 1x1 conv to project residual
         self.shortcut = (
             nn.Conv2d(in_features, out_features, kernel_size=1)
             if in_features != out_features else nn.Identity()
         )
+        self.layer_scaler = LayerScaler(layer_scaler_init_value, out_features)
+        self.drop_path = StochasticDepth(drop_p, mode="batch")
 
+        
     def forward(self, x: Tensor) -> Tensor:
         res = self.shortcut(x)
         x = self.block(x)
+        x = self.layer_scaler(x)
+        x = self.drop_path(x)
         x += res
         return x
     
-import torch
 
+"""
 #Check if the code above works    
-
 x = torch.rand(1, 32, 7, 7)
-block = BottleNeck(32, 62)
-print(block(x).shape)
+block = BottleNeck(32, 64)
+"""
+
 #Code to define a stage, which is a sequence of bottleneck blocks that reduce in size as stages progress by a factor of 2.
 class ConvNexStage(nn.Sequential):
     def __init__(
@@ -108,10 +119,9 @@ class ConvNexStage(nn.Sequential):
 
 #Check staging class works as intended
 """
-stage = ConvNexStage(32, 62, depth=1)
+stage = ConvNexStage(32, 64, depth=1)
 stage(torch.randn(1, 32, 14, 14)).shape
 """
-
 #Simulates the first layer in the model that does the heavy downsampling of the input image.
 class ConvNextStem(nn.Sequential):
     def __init__(self, in_features: int, out_features: int):
@@ -130,34 +140,60 @@ class ConvNextEncoder(nn.Module):
         stem_features: int,
         depths: List[int],
         widths: List[int],
+        drop_p: float = 0.0,
     ):
         super().__init__()
         self.stem = ConvNextStem(in_channels, stem_features)
 
         in_out_widths = list(zip(widths, widths[1:]))
-
+        # create drop paths probabilities (one for each stage)
+        drop_probs = [x.item() for x in torch.linspace(0, drop_p, sum(depths))] 
+        
         self.stages = nn.ModuleList(
             [
-                ConvNexStage(stem_features, widths[0], depths[0]),
+                ConvNexStage(stem_features, widths[0], depths[0], drop_p=drop_probs[0]),
                 *[
-                    ConvNexStage(in_features, out_features, depth)
-                    for (in_features, out_features), depth in zip(
-                        in_out_widths, depths[1:]
+                    ConvNexStage(in_features, out_features, depth, drop_p=drop_p)
+                    for (in_features, out_features), depth, drop_p in zip(
+                        in_out_widths, depths[1:], drop_probs[1:]
                     )
                 ],
             ]
         )
+        
 
     def forward(self, x):
         x = self.stem(x)
         for stage in self.stages:
             x = stage(x)
         return x
-
+"""
 #code to check if the ConvNextEncoder works as intended
 image = torch.rand(1, 3, 224, 224)
-encoder = ConvNextEncoder(in_channels=3, stem_features=64, depths=[3,3,9,3], widths=[256, 512, 1024, 2048])
-encoder(image).shape
+encoder = ConvNextEncoder(in_channels=3, stem_features=64, depths=[3,4,6,4], widths=[256, 512, 1024, 2048])
+print(encoder(image).shape)
+print(stage(torch.randn(1, 32, 14, 14)).shape)
+"""
+class ClassificationHead(nn.Sequential):
+    def __init__(self, num_channels: int, num_classes: int = 1000):
+        super().__init__(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(1),
+            nn.LayerNorm(num_channels),
+            nn.Linear(num_channels, num_classes)
+        )
+    
+    
+class ConvNextForImageClassification(nn.Sequential):
+    def __init__(self,  
+                 in_channels: int,
+                 stem_features: int,
+                 depths: List[int],
+                 widths: List[int],
+                 drop_p: float = .0,
+                 num_classes: int = 1000):
+        super().__init__()
+        self.encoder = ConvNextEncoder(in_channels, stem_features, depths, widths, drop_p)
+        self.head = ClassificationHead(widths[-1], num_classes)
 
-stage = ConvNexStage(32, 62, depth=1)
-stage(torch.randn(1, 32, 14, 14)).shape
+
