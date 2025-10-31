78884799cdb3073a5f8889ade8d1e6461c291e65
NatanCan <nathanc89614@gmail.com>
2025-10-31 15:03:06 +1000

    Added very little docstrings to explain each code block. Since this file also has the same train.py issue of no new defined functions, only comment segments were placed to explain.

--- recognition/s4703685-ConvNeXtADNI/predict.py
+++ recognition/s4703685-ConvNeXtADNI/predict.py
@@ -6,6 +6,7 @@ from PIL import Image
 from modules import ADNIConvNeXt
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
+#Set up the test data loader for model evaluation.
 batch_size = 256
 path = r"C:\Users\Nathan\Documents\AD_NC"
 ADNIDset = ADNIDataset(img_dir=r"C:\Users\Nathan\Documents\AD_NC", b_size=batch_size)
@@ -17,7 +18,7 @@ print("Checking for GPU...")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
-# Load model
+# Load model of the trained model to test accuracy
 model = ADNIConvNeXt().to(device)
 model.load_state_dict(torch.load(
     r"C:\Users\Nathan\Documents\COMP3710\COMP3710ReportCode\checkpoints\convnext_adni.pth",
